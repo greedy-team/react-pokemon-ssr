e23d8dbcdbfbf4ce62084007a37bedd6f1a6372b
rahwan10 <donggeon10@naver.com>
2026-08-04 18:51:42 +0900

    fix: 클라이언트 엔트리에서 style추가, StricMode적용 및 main.tsx삭제

--- csr/src/entry-client.tsx
+++ csr/src/entry-client.tsx
@@ -1,10 +1,14 @@
+import { StrictMode } from 'react';
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles/global.css";
 
 hydrateRoot(
   document.getElementById("root")!,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
