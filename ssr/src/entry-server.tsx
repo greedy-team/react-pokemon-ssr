6e0af5f98e10f158f9d3c5f41f30f5f483d9a50e
gyuminJJANG <ggavwu789@gmail.com>
2026-08-08 01:43:17 +0900

    feat: entry-server renderToPipeableStream 적용 (step7)

--- ssr/src/entry-server.tsx
+++ ssr/src/entry-server.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
-import { renderToString } from "react-dom/server";
+import {
+  renderToPipeableStream,
+  type RenderToPipeableStreamOptions,
+} from "react-dom/server";
 import { StaticRouter } from "react-router";
 import App from "./App";
 
-export function render(url: string, initialData?: any) {
-  const html = renderToString(
+export function render(
+  url: string,
+  initialData?: any,
+  options?: RenderToPipeableStreamOptions,
+) {
+  return renderToPipeableStream(
     <StrictMode>
       <StaticRouter location={url}>
         <App initialData={initialData} />
       </StaticRouter>
     </StrictMode>,
+    options,
   );
-
-  return { html };
 }
