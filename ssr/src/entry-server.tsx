ffb4948ed21a7684a23bf663726ef07abb6679bc
gyuminJJANG <ggavwu789@gmail.com>
2026-08-07 21:20:02 +0900

    feat: entry-server render 함수에 initialData 전달 파라미터 추가 (step5)

--- ssr/src/entry-server.tsx
+++ ssr/src/entry-server.tsx
@@ -3,11 +3,11 @@ import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
 import App from "./App";
 
-export function render(url: string) {
+export function render(url: string, initialData?: any) {
   const html = renderToString(
     <StrictMode>
       <StaticRouter location={url}>
-        <App />
+        <App initialData={initialData} />
       </StaticRouter>
     </StrictMode>,
   );
